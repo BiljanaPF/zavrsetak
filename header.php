7e1cc53d3cceb1f8bdfd4f32d32c2ba497dd8406
BiljanaPF <116907398+BiljanaPF@users.noreply.github.com>
2022-11-03 19:15:55 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,20 @@
-<?php
-
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
+<header>
+    <div class="blog-masthead">
+        <div class="container">
+            <nav class="nav">
+                <a class="nav-link active" href="#">Home</a>
+                <a class="nav-link" href="#">New features</a>
+                <a class="nav-link" href="#">Press</a>
+                <a class="nav-link" href="#">New hires</a>
+                <a class="nav-link" href="#">About</a>
+            </nav>
+        </div>
+    </div>
+​
+    <div class="blog-header">
+        <div class="container">
+            <h1 class="blog-title">The Bootstrap Blog</h1>
+            <p class="lead blog-description">An example blog template built with Bootstrap.</p>
+        </div>
+    </div>
+</header>
